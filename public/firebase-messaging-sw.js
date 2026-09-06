add1eaca24ad02b6ff2cde91bb1143780f671a8e
Subhankar Basak <subhankarbasakmail@gmail.com>
2026-09-07 01:29:52 +0530

    firebase credentials added

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,13 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js')
 // // Initialize the Firebase app in the service worker by passing the generated config
 
 const firebaseConfig = {
-    apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    authDomain: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    projectId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    storageBucket: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    messagingSenderId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
-    measurementId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  apiKey: "AIzaSyAOKwxjMQxIm6iETNaKlaa6uYEeG5UrjY0",
+  authDomain: "eclassify-3800d.firebaseapp.com",
+  projectId: "eclassify-3800d",
+  storageBucket: "eclassify-3800d.firebasestorage.app",
+  messagingSenderId: "845338884833",
+  appId: "1:845338884833:web:8bc665eebd7cf2f1c0d5a0",
+  measurementId: "G-886SKFP34T"
 };
 
 firebase?.initializeApp(firebaseConfig)
